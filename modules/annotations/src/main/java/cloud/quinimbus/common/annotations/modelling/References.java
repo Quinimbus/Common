a8cde4d1e78ab7a49da298d75c16ee9c21803d2e
Marcel Witte <marcel@aliquis.de>
2024-08-16 10:32:06 +0200

    added @References annotation for modelling

---
+++ modules/annotations/src/main/java/cloud/quinimbus/common/annotations/modelling/References.java
@@ -0,0 +1,12 @@
+package cloud.quinimbus.common.annotations.modelling;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface References {
+    Class<?> value();
+}
