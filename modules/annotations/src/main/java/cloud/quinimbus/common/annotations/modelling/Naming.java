e4d87e8086aad9c9c2c32ffd0bc1d1a64515767a
Marcel Witte <marcel@aliquis.de>
2024-09-24 14:15:29 +0200

    added @Naming annotation moved from Persistence

---
+++ modules/annotations/src/main/java/cloud/quinimbus/common/annotations/modelling/Naming.java
@@ -0,0 +1,10 @@
+package cloud.quinimbus.common.annotations.modelling;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Naming {}
